e7d8efc6b21ff2cc47ade1ef7acdbbad6daa1422
alanruttenberg@gmail.com <alanruttenberg@gmail.com@b0be31cf-4e2f-eb6c-0eff-db977a60bcf5>
2012-02-12 01:49:28 +0000

    starting r21 demo

---
+++ trunk/src/tools/label-source.lisp
@@ -0,0 +1,36 @@
+(defparameter *ohd-label-source* nil)
+
+(defmethod make-uri-from-label-source ((source (eql :ohd)) name actual)
+  "URI label source for ohd. All labels in OHD or imports are available. Case insensitive matching. Complains if an ambiguous label is used, or if a label is missing, or if actual is supplied and doesn't match the looked up term"
+  (unless *ohd-label-source*
+    (let ((table (make-hash-table :test 'equalp)))
+      (loop for file in '("ohd:imports;fma-jaws-teeth.owl")
+	   do
+	   (let ((kb (load-ontology file)))
+	     (let ((labels (rdfs-labels kb)))
+	       (maphash (lambda(uri label) 
+			  (let ((label (car label)))
+			    (if (gethash label table) 
+				(unless (eq (gethash label table)  uri)
+				  (print-db (gethash label table) uri label)
+				  (setf (gethash label table) :ambiguous))
+				(setf (gethash label table) uri))))
+			labels))))
+      (setq *ohd-label-source* table)))
+  (let ((found (gethash name *ohd-label-source*)))
+    (when (eq found :ambiguous)
+      (progn
+	(warn "Uri label ~a in ~a is ambiguous" name source)
+	(setq found nil)))
+    (if found
+	(progn
+	  (when actual
+	    (assert (eq found (make-uri nil actual)) (found actual)
+		    "Uri label lookup for '~a' - ~a doesn't match specified actual '~a'"
+		    name found actual))
+	  found)
+	(if actual
+	    (progn
+	      (warn "Uri label lookup for '~a' failed - using provided actual: '~a'" name actual)
+	      (make-uri nil actual))
+	    (error "Couldn't determine which URI was meant by '~a' in ~a" name source)))))
---
+++ trunk/src/tools/queries.lisp
@@ -0,0 +1,11 @@
+(defun demo()
+  (let ((teeth (load-ontology (to-iri "ohd:imports;fma-jaws-teeth.owl") :reasoner :pellet))) 
+    (sparql '(:select (?label ?tooth) () 
+	      (?tooth !rdfs:subClassOf !'Tooth'@ohd)
+	      (?tooth !rdfs:subClassOf (!'part'@ohd some !'Buccal surface enamel of tooth'@ohd))
+	      (?tooth !rdfs:label ?label)
+	      ) :kb teeth
+		:syntax :terp
+		:use-reasoner :pellet
+		:trace "teeth with Buccal surfaces"
+		:values nil)))
