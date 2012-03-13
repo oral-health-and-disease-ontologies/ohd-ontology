254f62535750f389a14603ceaadfd5a259693dc6
wdduncan@gmail.com <wdduncan@gmail.com@b0be31cf-4e2f-eb6c-0eff-db977a60bcf5>
2012-03-13 19:51:34 +0000

    Incorporated the use of r21-utilities.

--- trunk/src/tools/eaglesoft-dental-patients.lisp
+++ trunk/src/tools/eaglesoft-dental-patients.lisp
@@ -4,7 +4,7 @@
 ;; global variable used for to salt the md5 checksum
 (defparameter *salt* nil)
 
-;; ensure that sql libary is loadd
+;; ensure that sql libary is loaded
 (add-to-classpath "/Applications/SQLAnywhere12/System/java/sajdbc4.jar")
 
 (defun get-dental-patient-ont (&key iri ont-iri)
@@ -26,10 +26,8 @@
     ;; set global variables 
     (setf *iri* iri)
     
-    ;; set up connection string and query. Put password in ~/.pattersondbpw
-    (setf url (concatenate 'string 
-			   "jdbc:sqlanywhere:Server=PattersonPM;UserID=PDBA;password="
-			   (with-open-file (f "~/.pattersondbpw") (read-line f))))
+    ;; set up connection string and query.
+    (setf url (get-eaglesoft-database-url))
 
     ;; get query string for amalgam restorations
     (setf query (get-dental-patients-query))
@@ -143,39 +141,6 @@
     ;; return axioms
     axioms))
 
-(defun get-unique-iri(string &key salt class-type iri-base args)
-  "Returns a unique iri by doing a md5 checksum on the string parameter. An optional md5 salt value is specified by the salt key value (i.e., :salt salt). The class-type argument (i.e., :class-type class-type) concatentates the class type to the string.  This parameter is highly suggested, since it helps guarntee that ire will be unique.  The iri-base (i.e., :iri-base iri-base) is prepended to the iri.  For example, (get-unique-iri \"test\" :iri-base \"http://test.com/\" will prepend \"http://test.com/\" to the md5 checksum of \"test\".  The args parmameter is used to specify an other information you wish to concatenate to the string paramenter.  Args can be either a single value or list; e.g., (get-unique-iri \"test\" :args \"foo\") (get-unique-iri \"test\" :args '(\"foo\" \"bar\")."
-  ;; check that string param is a string
-  (if (not (stringp string)) 
-      (setf string (format nil "~a" string)))
-
-  ;; prepend class type to string
-  (when class-type
-    (setf class-type (remove-leading-! class-type))
-    (setf string (format nil "~a~a" class-type string)))
-  
-  ;; concatenate extra arguments to string
-  (when args
-    (cond 
-      ((listp args)
-       (loop for item in args do
-	    (setf item (remove-leading-! item))
-	    (setf string (format nil "~a~a" string item))))
-      (t (setf string (format nil "~a~a" string args)))))
-
-  ;; encode string
-  (setf string (encode string :salt salt))
-  (when iri-base
-    (setf string (format nil "~a~a" iri-base string)))
-  (make-uri string))
-
-(defun remove-leading-! (iri)
-  "Removes the leading '!' from a iri and returns the iri as a string.
-If no leading '!' is present, the iri is simply returned as string."
-  (setf iri (format nil "~a" iri))
-  (when (equal "!" (subseq iri 0 1)) (setf iri (subseq iri 1)))
-  ;; return iri
-  iri)
 
 (defun get-dental-patients-query ()
 "
@@ -188,47 +153,5 @@ FROM
 WHERE LENGTH(birth_date) > 0
 ORDER BY patient_id")
 
-(defun get-eaglesoft-salt ()
-  "Returns the salt to be used with the md5 checksum.  
-Note: The .pattersondbpw file is required to run this procedure."
-  (with-open-file (f "~/.pattersondbpw") (read-line f)))
-    
 
-(defun encode (string &key salt)
-  "Returns and md5 checksum of the string argument.  When the salt argument is present, it used in the computing of the md5 check sum."
-  (let ((it nil))
-    ;; check for salt
-    (when salt
-      (setf string (format nil "~a~a" salt string)))
-    
-    (setf it (new 'com.hp.hpl.jena.shared.uuid.MD5 string))
-    
-    (#"processString" it)
-    (#"getStringDigest" it)))
 
-(defun str-right (string num)
-	   (let ((end nil)
-		 (start nil))
-	     (setf end (length string))
-	     (setf start (- end num))
-	     (when (>= start 0)
-	       (setf string (subseq string start end)))))
-
-(defun str-left (string num)
-  (when (<= num (length string))
-    (setf string (subseq string 0 num))))
-
-(defun str+ (&rest values)
-  "A shortcut for concatenting a list of strings. Code found at:
-http://stackoverflow.com/questions/5457346/lisp-function-to-concatenate-a-list-of-strings
-Parameters:
-  values:  The string to be concatenated.
-Usage:
-  (str+ \"string1\"   \"string1\" \"string 3\" ...)
-  (str+ s1 s2 s3 ...)"
-
-  ;; the following make use of the format functions
-  ;;(format nil "~{~a~}" values)
-  ;;; use (format nil "~{~a^ ~}" values) to concatenate with spaces
-  ;; this may be the simplist to understand...
-  (apply #'concatenate 'string values))
