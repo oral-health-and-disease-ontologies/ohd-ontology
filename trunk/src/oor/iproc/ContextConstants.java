98099b2a20e3d656c825d15991ff9dba9fba4aeb
NickLondhe@gmail.com <NickLondhe@gmail.com@b0be31cf-4e2f-eb6c-0eff-db977a60bcf5>
2012-03-27 23:18:30 +0000

    Chain of command commits - 1

---
+++ trunk/src/oor/iproc/ContextConstants.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package oor.iproc;
+
+/**
+ * @author nikhillo
+ *
+ */
+public interface ContextConstants {
+	public static final String __REV__ = "$Id$ $Date$ $Author$";
+	
+	/** Generic **/
+	public static final String SRC_IMG = "source";
+	public static final String TGT_IMG = "target";
+	public static final String SRC_FEATURES = "srcFeatures";
+	public static final String TGT_FEATURES = "tgtFeatures";
+
+	/** Subtract background (SB) **/
+	public static final String SB_RADIUS = "radius";
+	public static final String SB_CREATEBG = "createBackground";
+	public static final String SB_LIGHTBG = "lightBackground";
+	public static final String SB_USEPARA = "useParaboloid";
+	public static final String SB_PRESMOOTH = "doPresmooth";
+	public static final String SB_CORRCORN = "correctCorners";
+	
+	/** Morphology (MP) **/
+	public static final String MP_TYPE = "type";
+	public static final String MP_RADIUS = "radius";
+	public static final String MP_CMD = "command";
+	
+	/** MOPS features (MOF) **/
+	public static final String MOF_FDSZ = "fdSize";
+	public static final String MOF_INITSIG = "initialSigma";
+	public static final String MOF_MAXOCT = "maxOctaveSize";
+	public static final String MOF_MINOCT = "minOctaveSize";
+	public static final String MOF_STEPS = "steps";
+	
+	/** SIFT features (SIF) **/
+	public static final String SIF_FDBINS = "fdBins";
+	public static final String SIF_FDSZ = "fdSize";
+	public static final String SIF_INITSIG = "initialSigma";
+	public static final String SIF_MAXOCT = "maxOctaveSize";
+	public static final String SIF_MINOCT = "minOctaveSize";
+	public static final String SIF_STEPS = "steps";
+}
---
+++ trunk/src/oor/iproc/cmd/MOPSExtractFeaturesCmd.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package oor.iproc.cmd;
+
+import java.util.ArrayList;
+
+import mpicbg.ij.MOPS;
+import mpicbg.imagefeatures.Feature;
+import mpicbg.imagefeatures.FloatArray2DMOPS;
+import mpicbg.imagefeatures.FloatArray2DMOPS.Param;
+
+import org.apache.commons.chain.Context;
+
+/**
+ * @author nikhillo
+ *
+ */
+public class MOPSExtractFeaturesCmd extends AbstractCommand {
+	@SuppressWarnings("unused")
+	private static final String __REV__ = "$Id$ $Date$ $Author$";
+	
+	/* (non-Javadoc)
+	 * @see oor.iproc.cmd.AbstractCommand#execute(org.apache.commons.chain.Context)
+	 */
+	@Override
+	public boolean execute(Context ctxt) throws Exception {
+		Param p = new Param();
+		p.fdSize = getIntValue(ctxt, MOF_FDSZ);
+		p.initialSigma = getFLoatValue(ctxt, MOF_INITSIG);
+		p.maxOctaveSize = getIntValue(ctxt, MOF_MAXOCT);
+		p.minOctaveSize = getIntValue(ctxt, MOF_MINOCT);
+		p.steps = getIntValue(ctxt, MOF_STEPS);
+		MOPS mops = new MOPS(new FloatArray2DMOPS(p));
+		ArrayList<Feature> features = new ArrayList<Feature>();
+		mops.extractFeatures(getTarget(ctxt), features);
+		setTargetFeatures(ctxt, features);
+		return false;
+	}
+
+}
