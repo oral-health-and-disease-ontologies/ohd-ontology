92c46ebf4d88c36abc91170b505411541e90c09a
wdduncan <wdduncan@gmail.com>
2012-04-09 20:22:34 +0000

    Added methods for ceating endodontic ontology.

--- src/tools/eaglesoft-endodontics.lisp
+++ src/tools/eaglesoft-endodontics.lisp
@@ -6,14 +6,14 @@
 
 ;;****************************************************************
 ;; Database preparation: 
-;; When get-eaglesoft-surgical-extractsions-ont is ran, the program verifies that the 
+;; When get-eaglesoft-endodontics-ont is ran, the program verifies that the 
 ;; action_codes and patient_history tables exist.  This is done by calling 
 ;; prepare-eaglesoft-db.  However, this only tests that these tables exist in the 
 ;; user's database. If these table need to be recreated, the call 
 ;; get-eaglesoft-fillings-ont with :force-create-table key set to t.
 
-(defun get-eaglesoft-surgical-extractions-ont (&key force-create-table)
-  "Returns an ontology of the surgical extractions contained in the Eaglesoft database.  They force-create-table key is used to force the program to recreate the actions_codes and patient_history tables."
+(defun get-eaglesoft-endodontics-ont (&key force-create-table)
+  "Returns an ontology of the endodontic procedures contained in the Eaglesoft database.  They force-create-table key is used to force the program to recreate the actions_codes and patient_history tables."
   (let ((connection nil)
 	(statement nil)
 	(results nil)
@@ -29,11 +29,11 @@
     (prepare-eaglesoft-db url :force-create-table force-create-table)
 
     ;; get query string for restorations
-    (setf query (get-eaglesoft-surgical-extractions-query))
+    (setf query (get-eaglesoft-endodontics-query))
 
     (with-ontology ont (:collecting t 
-			:base *eaglesoft-individual-surgical-extractions-iri-base* 
-			:ontology-iri *eaglesoft-surgical-extractions-ontology-iri*)
+			:base *eaglesoft-individual-endodontics-iri-base*
+			:ontology-iri *eaglesoft-endodontics-ontology-iri*)
 	((unwind-protect
 	      (progn
 		;; connect to db and get data
@@ -59,7 +59,7 @@
 			    (#"getString" results "tran_date")))
 		     
 		     ;; get axioms
-		     (as (get-eaglesoft-surgical-extraction-axioms 
+		     (as (get-eaglesoft-endodontic-axioms 
 		     	  (#"getString" results "patient_id")
 		     	  occurrence-date
 		     	  (#"getString" results "tooth_data")
@@ -75,13 +75,12 @@
       ;; return the ontology
       (values ont count))))
 
-(defun get-eaglesoft-surgical-extraction-axioms 
+(defun get-eaglesoft-endodontic-axioms 
     (patient-id occurrence-date tooth-data ada-code record-count)
   (let ((axioms nil)
 	(patient-uri nil)
-	(extraction-role-uri nil)
-	(extraction-procedure-uri nil)
-	(procedure-type-uri nil)
+	(endodontic-role-uri nil)
+	(endodontic-procedure-uri nil)
 	(tooth-name nil)
 	(tooth-uri nil)
 	(tooth-type-uri nil)
@@ -112,60 +111,60 @@
 				      ,(str+ tooth-name
 					     " of patient " patient-id)) axioms)
 	 
-       ;; declare instance of !ohd:'tooth to be extracted role'
-	 (setf extraction-role-uri
-	       (get-eaglesoft-tooth-to-be-extracted-role-iri patient-id tooth record-count))
+       ;; declare instance of !ohd:'tooth to undergo endodontic procedure role'
+	 (setf endodontic-role-uri
+	       (get-eaglesoft-tooth-to-undergo-endodontic-procedure-role-iri
+		patient-id tooth record-count))
 		
-	 (push `(declaration (named-individual ,extraction-role-uri)) axioms)
-	 (push `(class-assertion !'tooth to be extracted role'@ohd ,extraction-role-uri) axioms)
+	 (push `(declaration (named-individual ,endodontic-role-uri)) axioms)
+	 (push `(class-assertion !'tooth to undergo endodontic procedure role'@ohd 
+				 ,endodontic-role-uri) axioms)
 
-	 ;; add annotation about 'tooth to be extracted role'
+	 ;; add annotation about 'tooth to undergo endodontic procedure role'
 	 (push `(annotation-assertion !rdfs:label 
-				      ,extraction-role-uri
-				      ,(str+ "tooth to be extracted role for " 
+				      ,endodontic-role-uri
+				      ,(str+ "tooth to undergo endodontic procedure role for " 
 					     tooth-name " of patient " 
 					     patient-id)) axioms)
 
 
-         ;; declare instance of extraction procedure
-	 (setf procedure-type-uri !'tooth extraction procedure'@ohd)
-	 (setf extraction-procedure-uri
-	       (get-eaglesoft-tooth-extraction-procedure-iri
-		patient-id tooth-name procedure-type-uri record-count))
+         ;; declare instance of endodontic procedure
+	 (setf endodontic-procedure-uri
+	       (get-eaglesoft-endodontic-procedure-iri patient-id tooth-name record-count))
 	 
-	 (push `(declaration (named-individual ,extraction-procedure-uri)) axioms)
-	 (push `(class-assertion !'tooth extraction procedure'@ohd 
-				 ,extraction-procedure-uri) axioms)
+	 (push `(declaration (named-individual ,endodontic-procedure-uri)) axioms)
+	 (push `(class-assertion !'endodontic procedure'@ohd 
+				 ,endodontic-procedure-uri) axioms)
 
-	 ;; add annotation about this extraction procedure
+	 ;; add annotation about this endodontic procedure
 	 (push `(annotation-assertion !rdfs:label 
-				      ,extraction-procedure-uri
-				      ,(str+ "tooth extraction procedure performed on tooth " 
+				      ,endodontic-procedure-uri
+				      ,(str+ "endodontic procedure performed on tooth " 
 					     tooth-name " in patient " 
 					     patient-id)) axioms)
 
-	 ;; add data property !ohd:'occurence date' to restoration
+	 ;; add data property !ohd:'occurence date' to endodontic procedure
 	 (push `(data-property-assertion !'occurence date'@ohd
-					 ,extraction-procedure-uri
+					 ,endodontic-procedure-uri
 					 (:literal ,occurrence-date !xsd:date)) axioms)
 	  ;;;; relate instances ;;;;
 	 
-         ;; 'tooth to be extracted role' inheres in tooth
+         ;; 'tooth to undergo endodontic procedure role' inheres in tooth
 	 (push `(object-property-assertion !'inheres in'@ohd
-					   ,extraction-role-uri ,tooth-uri) axioms)
+					   ,endodontic-role-uri ,tooth-uri) axioms)
 
-         ;; 'tooth extraction procedure' realizes 'tooth to be extracted role'
+         ;; 'endodontic procedure' realizes 'tooth to undergo endodontic procedure role'
 	 (push `(object-property-assertion !'realizes'@ohd
-					   ,extraction-procedure-uri
-					   ,extraction-role-uri) axioms)
+					   ,endodontic-procedure-uri
+					   ,endodontic-role-uri) axioms)
 	 
-         ;; 'tooth extraction procedure' has particpant tooth
+         ;; 'endodontic procedure' has particpant tooth
 	 (push `(object-property-assertion !'has participant'@ohd
-					   ,extraction-procedure-uri ,tooth-uri) axioms)
+					   ,endodontic-procedure-uri ,tooth-uri) axioms)
 
-         ;;  'tooth extraction procedure' has particpant patient
+         ;;  'endodontic procedure' has particpant patient
 	 (push `(object-property-assertion !'has participant'@ohd
-					   ,extraction-procedure-uri ,patient-uri) axioms)
+					   ,endodontic-procedure-uri ,patient-uri) axioms)
 	 ) ;; end loop
     
     ;;(pprint axioms)
@@ -173,56 +172,77 @@
     ;; return axioms
     axioms))
 
-(defun get-eaglesoft-tooth-to-be-extracted-role-iri (patient-id tooth-name instance-count)
-  "Returns an iri for a 'tooth to be extracted role' that is generated by the patient id, the name of the type of the tooth, and a count variable that used differientiate tooth role intances that have the same patient-id/tooth-name but are numerically distinct."
+(defun get-eaglesoft-tooth-to-undergo-endodontic-procedure-role-iri 
+    (patient-id tooth-name instance-count)
+  "Returns an iri for a 'tooth to undergo endodontic procedure role' that is generated by the patient id, the name of the type of the tooth, and a count variable that used differientiate tooth role intances that have the same patient-id/tooth-name but are numerically distinct."
   (let ((uri nil))
     (setf uri 
 	  (get-unique-individual-iri patient-id 
 				     :salt *eaglesoft-salt*
 				     :iri-base *eaglesoft-individual-teeth-iri-base*
-				     :class-type !'tooth to be extracted role'@ohd
+				     :class-type !'tooth to undergo endodontic procedure role'@ohd
 				     :args `(,tooth-name ,instance-count "eaglesoft")))
     ;; return uri
     uri))
 
-(defun get-eaglesoft-tooth-extraction-procedure-iri (patient-id tooth-name
-						     procedure-type-iri instance-count)
-  "Returns an iri for a tooth extraction procedure identified is generated by the patient id, the name of the type of tooth, the type of procedure, and a count variable that used differientiate procedure intances that have the same patient-id/restoration-type-iri but are numerically distinct."
+(defun get-eaglesoft-endodontic-procedure-iri (patient-id tooth-name instance-count)
+  "Returns an iri for an endodontic procedure is generated by the patient id, the name of the type of tooth, and a count variable that used differientiate procedure intances that have the same patient-id/restoration-type-iri but are numerically distinct."
   (let ((uri nil))
     (setf uri 
 	  (get-unique-individual-iri patient-id 
 				     :salt *eaglesoft-salt*
-				     :iri-base *eaglesoft-individual-surgical-extractions-iri-base*
-				     :class-type procedure-type-iri
+				     :iri-base *eaglesoft-individual-endodontics-iri-base*
+				     :class-type !'endodontic procedure'@ohd
 				     :args `(,tooth-name ,instance-count "eaglesoft")))
     ;; return uri
     uri))
 
-(defun get-eaglesoft-surgical-extractions-query ()
+(defun get-eaglesoft-endodontics-query ()
 "
 /*
-Returns surgical extraction records.
+Returns endodontic procedure records.
 Note: This has not been filtered for primary (baby) teeth.
-1215 records returned
+1436 records returned
 */
 
-SET rowcount 0 
+SET rowcount 0
 
 SELECT
   *
 FROM
   patient_history
 WHERE
- length(tooth_data) > 31
-AND (
-    ada_code IN ('D7110',
-                 'D7120',
-                 'D7140',
-                 'D7210')
-  OR -- older codes begin with a '0'
-    ada_code IN ('07110',
-                 '07120',
-                 '07140',
-                 '07210'))
+  (
+    ada_code IN ('D3220',
+                 'D3221',
+                 'D3222',
+                 'D3310',
+                 'D3320',
+                 'D3330',
+                 'D3346',
+                 'D3347',
+                 'D3348',
+                 'D3410',
+                 'D3421',
+                 'D3425',
+                 'D3450',
+                 'D3920' )
+  OR
+    -- Older codes begin with a '0'
+    ada_code IN ('03220',
+                 '03221',
+                 '03222',
+                 '03310',
+                 '03320',
+                 '03330',
+                 '03346',
+                 '03347',
+                 '03348',
+                 '03410',
+                 '03421',
+                 '03425',
+                 '03450',
+                 '03920' ) )
+AND length(tooth_data) > 31
 "
 )
