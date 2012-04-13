b5a40c8f7fefc12cda019aff9711107e88f135bd
alanruttenberg@gmail.com <alanruttenberg@gmail.com@b0be31cf-4e2f-eb6c-0eff-db977a60bcf5>
2012-04-13 18:14:20 +0000

    figured out how to specify reasoning for http (https://groups.google.com/a/clarkparsia.com/group/stardog/browse_thread/thread/f7ed32487f59513f/0b2884013d1d43de)  and included an example in the test script.

--- trunk/src/tools/test-stardog-r21.sh
+++ trunk/src/tools/test-stardog-r21.sh
@@ -41,6 +41,10 @@ stardog query -c "http://127.0.0.1:5822/r21db;reasoning=$expressivity" -q "PREFI
 # # ask for types of tooth14
 # stardog query -c "http://127.0.0.1:5822/r21db;reasoning=$expressivity" -q "PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT * WHERE { <http://purl.obolibrary.org/obo/OHD_0000016> rdf:type ?t. optional {?t rdfs:label ?l} }"
 
+# test sparql endpoint over http
+echo "testing sparql over http"
+wget --quiet --header='accept: application/sparql-results+json' -O - 'http://127.0.0.1:5822/r21db;reasoning=$expressivity/query?query=select%20*%20where%20{?s%20?p%20?o}%20limit%2010&'
+
 # stop the server
 #stardog-admin server stop
 
