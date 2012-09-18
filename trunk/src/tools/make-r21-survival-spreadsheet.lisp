b7609e40cbe477de70604876ae66f4c6e81e9af6
wdduncan@gmail.com <wdduncan@gmail.com@b0be31cf-4e2f-eb6c-0eff-db977a60bcf5>
2012-09-18 03:26:33 +0000

    updating query progress

--- trunk/src/tools/make-r21-survival-spreadsheet.lisp
+++ trunk/src/tools/make-r21-survival-spreadsheet.lisp
@@ -70,7 +70,7 @@
 			     ?surface
 			     ?material
 			     ) 
-		    (:limit 10 :order-by (?person ?toothn ?proceduredate) )
+		    (:limit 10 :order-by (?person ?toothn))
 
 		    ;; billd: general not about 'asserted type'
 		    ;; the 'assert type' relation is used to restrict matching so that
@@ -85,8 +85,8 @@
 		    (?persontype !rdfs:subClassOf !'dental patient'@ohd)
 		    ;;(?personi !rdf:type !'homo sapiens'@ohd) 
 		    (?personi !'asserted type'@ohd ?persontype) 
-		    (?personi !rdfs:label ?person) ; their label 
-		    (?persontype !rdfs:label ?sex) ; their sex
+		    (?personi !rdfs:label ?person)	; their label 
+		    (?persontype !rdfs:label ?sex)	; their sex
 		    (?personi !'birth_date'@ohd ?bdate) ; their birth date
 		    
 		    ;; and the tooth that was worked on
@@ -161,27 +161,17 @@
 
 (defun test-owlim-query ()
   (r21query 
-   '(:select (?tooth ?toothtype ?class)
+   '(:select 
+     (?super)
      (:limit 10)
-     (?toothclass !rdfs:subClassOf !obo:FMA_12516)
-     (?toothclass !rdfs:label ?toothtype)
-     (?toothi !rdf:type ?toothclass)
-     (?toothi !rdfs:label ?tooth)
-     
-     
-     ;;(?toothtype !sesame:directSubClassOf ?class)
-     
-     ;; try find direct super class of tooth
-     ;; code adapted from: http://www.mail-archive.com/owlim-discussion@ontotext.com/msg01728.html
+     ;;(:bind !obo:FMA_55696 'as ?concept) DOESN'T WORK!
+     ;;(?concept !rdfs:subClassOf ?super)
+     (?concept !rdfs:subClassOf !'Tooth'@ohd)
      (:optional 
-      (?toothclass !rdfs:subClassOf ?class)
-      (?class !rdfs:subClassOf ?super)
-      (:filter (and (not (equal ?toothclass ?class)) (not (equal ?class ?super)))))
-     
-     ;;(:filter (not (and (bound ?class) (not (equal ?super ?toothclass)))))
-     (:filter (and (not (bound ?class)) (not (equal ?super ?toothclass))))
-
-   )
+      (?concept !rdfs:subClassOf ?inbetweener)
+      (:filter (and (not (equal ?inbetweener ?concept)) (not (equal ?inbetweener ?super)))))
+     (:filter (and (not (bound ?inbetweener)) (not (equal ?super ?concept))))
+     )
      
    :reasoner 'owlim-se-r21
    :trace "test owlim query")
@@ -190,19 +180,112 @@
 ;; try to work with query like this to get direct super class:
 ;; code adapted from: http://www.mail-archive.com/owlim-discussion@ontotext.com/msg01728.html
 
-SELECT ?tooth ?toothtype ?pclass
-WHERE { 
-   ?class rdfs:subClassOf obo:FMA_12516 . 
-   ?toothi rdf:type ?class .
+SELECT ?super {
+    BIND( obo:FMA_55696 as ?concept )
+    ?concept rdfs:subClassOf ?super .
+    OPTIONAL {
+    ?concept rdfs:subClassOf ?inbetweener .
+    ?inbetweener rdfs:subClassOf ?super .
+    FILTER( ?inbetweener != ?concept && ?inbetweener != ?super )
+  }
+  FILTER( ! BOUND(?inbetweener) && ?super != ?concept)
+}
 
-   ?class rdfs:label ?toothtype . 
-   ?toothi rdfs:label ?tooth .
-OPTIONAL { 
-  ?class rdfs:subClassOf ?pclass . 
-  ?pclass rdfs:subClassOf ?gpclass . 
-  FILTER (?pclass != ?class && ?pclass != ?gpclass)}.
-FILTER (! bound(?pclass) && ?gpclass != ?class)} LIMIT 10 
 |#
   
   ;; return nil
+  nil)
+
+(defun test2 ()
+  (r21query 
+   '(:select (?s ?l)
+     (:limit 10)
+     (?s !rdf:type !obo:FMA_12516)
+     (?s !rdfs:label ?l))
+   :reasoner 'owlim-se-r21
+   :trace "test2")
+
+   nil)
+
+(defun dental-materials-query ()
+  (sparql
+"
+PREFIX :<http://purl.obolibrary.org/obo/ohd/dev/ohd.owl#>
+PREFIX geo-pos:<http://www.w3.org/2003/01/geo/wgs84_pos#>
+PREFIX umbel-ac:<http://umbel.org/umbel/ac/>
+PREFIX sw-vocab:<http://www.w3.org/2003/06/sw-vocab-status/ns#>
+PREFIX ff:<http://factforge.net/>
+PREFIX music-ont:<http://purl.org/ontology/mo/>
+PREFIX dc-term:<http://purl.org/dc/terms/>
+PREFIX om:<http://www.ontotext.com/owlim/>
+PREFIX opencyc-en:<http://sw.opencyc.org/2008/06/10/concept/en/>
+PREFIX factbook:<http://www.daml.org/2001/12/factbook/factbook-ont#>
+PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX pext:<http://proton.semanticweb.org/protonext#>
+PREFIX dc:<http://purl.org/dc/elements/1.1/>
+PREFIX ot:<http://www.ontotext.com/>
+PREFIX onto:<http://www.ontotext.com/>
+PREFIX protege:<http://protege.stanford.edu/plugins/owl/protege#>
+PREFIX foaf:<http://xmlns.com/foaf/0.1/>
+PREFIX yago:<http://mpii.de/yago/resource/>
+PREFIX daml:<http://www.daml.org/2001/03/daml+oil#>
+PREFIX umbel:<http://umbel.org/umbel#>
+PREFIX pkm:<http://proton.semanticweb.org/protonkm#>
+PREFIX wordnet16:<http://xmlns.com/wordnet/1.6/>
+PREFIX swrlb:<http://www.w3.org/2003/11/swrlb#>
+PREFIX owl:<http://www.w3.org/2002/07/owl#>
+PREFIX example:<http://example.com/>
+PREFIX gr:<http://purl.org/goodrelations/v1#>
+PREFIX wordnet:<http://www.w3.org/2006/03/wn/wn20/instances/>
+PREFIX opencyc:<http://sw.opencyc.org/concept/>
+PREFIX ro:<http://www.obofoundry.org/ro/ro.owl#>
+PREFIX wordn-sc:<http://www.w3.org/2006/03/wn/wn20/schema/>
+PREFIX nytimes:<http://data.nytimes.com/>
+PREFIX dbp-prop:<http://dbpedia.org/property/>
+PREFIX owl2:<http://www.w3.org/2006/12/owl2#>
+PREFIX geonames:<http://sws.geonames.org/>
+PREFIX dcterms:<http://purl.org/dc/terms/>
+PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
+PREFIX swrl:<http://www.w3.org/2003/11/swrl#>
+PREFIX owl2xml:<http://www.w3.org/2006/12/owl2-xml#>
+PREFIX dbpedia:<http://dbpedia.org/resource/>
+PREFIX oasis:<http://psi.oasis-open.org/iso/639/#>
+PREFIX individuals:<http://purl.obolibrary.org/obo/ohd/individuals/>
+PREFIX geo-ont:<http://www.geonames.org/ontology#>
+PREFIX umbel-en:<http://umbel.org/umbel/ne/wikipedia/>
+PREFIX xsp:<http://www.owl-ontologies.com/2005/08/07/xsp.owl#>
+PREFIX bbc-pont:<http://purl.org/ontology/po/>
+PREFIX ptop:<http://proton.semanticweb.org/protontop#>
+PREFIX lingvoj:<http://www.lingvoj.org/ontology#>
+PREFIX fb:<http://rdf.freebase.com/ns/>
+PREFIX dbtune:<http://dbtune.org/bbc/peel/work/>
+PREFIX obo:<http://purl.obolibrary.org/obo/>
+PREFIX psys:<http://proton.semanticweb.org/protonsys#>
+PREFIX umbel-sc:<http://umbel.org/umbel/sc/>
+PREFIX dbp-ont:<http://dbpedia.org/ontology/>
+PREFIX ub:<http://www.lehigh.edu/~zhp2/2004/0401/univ-bench.owl#>
+PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>
+PREFIX oboInOwl:<http://www.geneontology.org/formats/oboInOwl#>
+PREFIX skos:<http://www.w3.org/2004/02/skos/core#>
+
+SELECT DISTINCT ?material ?year (COUNT(?material) as ?m)
+WHERE { 
+  ?materialtype rdfs:subClassOf obo:OHD_0000000 . # obo:OHD_0000000 -> 'restoration material'
+  ?materiali rdf:type ?materialtype .
+  #?materiali obo:OHD_0000092 ?materialtype . # obo:OHD_0000092 -> 'asserted type'
+  ?procedurei rdf:type obo:OHD_0000002 . # obo:OHD_0000002 -> 'dental procedure'
+  ?procedurei obo:BFO_0000057 ?materiali . # obo:BFO_0000057 -> 'has participant'
+  ?procedurei obo:OHD_0000015 ?proceduredate . # obo:0000015 -> 'occurrence date'
+  BIND (YEAR(?proceduredate) as ?year)
+  ?materialtype rdfs:label ?material . 
+FILTER (?materialtype != obo:OHD_0000000) # obo:OHD_0000000 -> 'restoration material'
+FILTER (?materialtype != obo:OHD_0000048) # obo:OHD_0000048 -> 'metal'
+}
+GROUP BY ?material ?year
+ORDER BY ?year
+"
+
+:use-reasoner (eval 'owlim-se-r21)
+:trace "dental materials")
+
   nil)
