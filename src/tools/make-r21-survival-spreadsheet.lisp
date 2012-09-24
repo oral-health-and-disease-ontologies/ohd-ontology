167cc2dd79d94be3eba1db86dc54248702370d07
wdduncan <wdduncan@gmail.com>
2012-09-24 22:10:26 +0000

    upading progress

--- src/tools/make-r21-survival-spreadsheet.lisp
+++ src/tools/make-r21-survival-spreadsheet.lisp
@@ -103,7 +103,7 @@
 		    ;; (:optional (?surfacei !'is part of'@ohd ?toothi)) ; surface instance is part of tooth instance
 		    ;; (:optional (?surfacei !rdfs:label ?surface)) ; get label of surface instance
 		    (:optional 
-		     (?surfacei !rdf:type !'Surface enamel of tooth'@ohd)
+ 		     (?surfacei !rdf:type !'Surface enamel of tooth'@ohd)
 		     (?surfacei !'is part of'@ohd ?toothi) ; surface instance is part of tooth instance
 		     (?surfacei !rdfs:label ?surface)) ; get label of surface instance
 		    
@@ -138,7 +138,7 @@
 		    
 		    ;; and the material used in the procedure
 		    (:optional
-		     (?materialtype !rdfs:subClassOf !'restoration material'@ohd)
+		     (?materialtype !rdfs:subClassOf !'dental restoration material'@ohd)
 		     (?materiali !rdf:type ?materialtype)
 		     (?procedurei !'has participant'@ohd ?materiali)
 		     (?materialtype !rdfs:label ?material))
@@ -270,22 +270,200 @@ PREFIX skos:<http://www.w3.org/2004/02/skos/core#>
 
 SELECT DISTINCT ?material ?year (COUNT(?material) as ?m)
 WHERE { 
-  ?materialtype rdfs:subClassOf obo:OHD_0000000 . # obo:OHD_0000000 -> 'restoration material'
+  ?materialtype rdfs:subClassOf obo:OHD_0000000 . # obo:OHD_0000000 -> 'dental restoration material'
   ?materiali rdf:type ?materialtype .
   #?materiali obo:OHD_0000092 ?materialtype . # obo:OHD_0000092 -> 'asserted type'
-  ?procedurei rdf:type obo:OHD_0000002 . # obo:OHD_0000002 -> 'dental procedure'
+  ?procedurei rdf:type obo:OHD_0000006 . # obo:OHD_0000006 -> 'filling restoration'
   ?procedurei obo:BFO_0000057 ?materiali . # obo:BFO_0000057 -> 'has participant'
   ?procedurei obo:OHD_0000015 ?proceduredate . # obo:0000015 -> 'occurrence date'
   BIND (YEAR(?proceduredate) as ?year)
   ?materialtype rdfs:label ?material . 
-FILTER (?materialtype != obo:OHD_0000000) # obo:OHD_0000000 -> 'restoration material'
+FILTER (?materialtype != obo:OHD_0000000) # obo:OHD_0000000 -> 'dental restoration material'
 FILTER (?materialtype != obo:OHD_0000048) # obo:OHD_0000048 -> 'metal'
 }
 GROUP BY ?material ?year
-ORDER BY ?year
+ORDER BY ?year ?material
 "
 
 :use-reasoner (eval 'owlim-se-r21)
 :trace "dental materials")
 
+  nil)
+
+(defun procedures-on-same-tooth-query ()
+
+  (sparql 
+"
+PREFIX :<http://purl.obolibrary.org/obo/ohd/dev/ohd.owl#>
+PREFIX geo-pos:<http://www.w3.org/2003/01/geo/wgs84_pos#>
+PREFIX umbel-ac:<http://umbel.org/umbel/ac/>
+PREFIX sw-vocab:<http://www.w3.org/2003/06/sw-vocab-status/ns#>
+PREFIX ff:<http://factforge.net/>
+PREFIX music-ont:<http://purl.org/ontology/mo/>
+PREFIX dc-term:<http://purl.org/dc/terms/>
+PREFIX om:<http://www.ontotext.com/owlim/>
+PREFIX opencyc-en:<http://sw.opencyc.org/2008/06/10/concept/en/>
+PREFIX factbook:<http://www.daml.org/2001/12/factbook/factbook-ont#>
+PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX pext:<http://proton.semanticweb.org/protonext#>
+PREFIX dc:<http://purl.org/dc/elements/1.1/>
+PREFIX ot:<http://www.ontotext.com/>
+PREFIX onto:<http://www.ontotext.com/>
+PREFIX protege:<http://protege.stanford.edu/plugins/owl/protege#>
+PREFIX foaf:<http://xmlns.com/foaf/0.1/>
+PREFIX yago:<http://mpii.de/yago/resource/>
+PREFIX daml:<http://www.daml.org/2001/03/daml+oil#>
+PREFIX umbel:<http://umbel.org/umbel#>
+PREFIX pkm:<http://proton.semanticweb.org/protonkm#>
+PREFIX wordnet16:<http://xmlns.com/wordnet/1.6/>
+PREFIX swrlb:<http://www.w3.org/2003/11/swrlb#>
+PREFIX owl:<http://www.w3.org/2002/07/owl#>
+PREFIX example:<http://example.com/>
+PREFIX gr:<http://purl.org/goodrelations/v1#>
+PREFIX wordnet:<http://www.w3.org/2006/03/wn/wn20/instances/>
+PREFIX opencyc:<http://sw.opencyc.org/concept/>
+PREFIX ro:<http://www.obofoundry.org/ro/ro.owl#>
+PREFIX wordn-sc:<http://www.w3.org/2006/03/wn/wn20/schema/>
+PREFIX nytimes:<http://data.nytimes.com/>
+PREFIX dbp-prop:<http://dbpedia.org/property/>
+PREFIX owl2:<http://www.w3.org/2006/12/owl2#>
+PREFIX geonames:<http://sws.geonames.org/>
+PREFIX dcterms:<http://purl.org/dc/terms/>
+PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
+PREFIX swrl:<http://www.w3.org/2003/11/swrl#>
+PREFIX owl2xml:<http://www.w3.org/2006/12/owl2-xml#>
+PREFIX dbpedia:<http://dbpedia.org/resource/>
+PREFIX oasis:<http://psi.oasis-open.org/iso/639/#>
+PREFIX individuals:<http://purl.obolibrary.org/obo/ohd/individuals/>
+PREFIX geo-ont:<http://www.geonames.org/ontology#>
+PREFIX umbel-en:<http://umbel.org/umbel/ne/wikipedia/>
+PREFIX xsp:<http://www.owl-ontologies.com/2005/08/07/xsp.owl#>
+PREFIX bbc-pont:<http://purl.org/ontology/po/>
+PREFIX ptop:<http://proton.semanticweb.org/protontop#>
+PREFIX lingvoj:<http://www.lingvoj.org/ontology#>
+PREFIX fb:<http://rdf.freebase.com/ns/>
+PREFIX dbtune:<http://dbtune.org/bbc/peel/work/>
+PREFIX obo:<http://purl.obolibrary.org/obo/>
+PREFIX psys:<http://proton.semanticweb.org/protonsys#>
+PREFIX umbel-sc:<http://umbel.org/umbel/sc/>
+PREFIX dbp-ont:<http://dbpedia.org/ontology/>
+PREFIX ub:<http://www.lehigh.edu/~zhp2/2004/0401/univ-bench.owl#>
+PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>
+PREFIX oboInOwl:<http://www.geneontology.org/formats/oboInOwl#>
+PREFIX skos:<http://www.w3.org/2004/02/skos/core#>
+
+select ?patient ?tooth ?surface ?procedure (count(*) as ?total)
+where {
+  ?patienti rdf:type obo:OHD_0000012 .
+  ?toothi rdf:type obo:FMA_12516 .
+  ?surfacei rdf:type obo:FMA_no_fmaid_Surface_enamel_of_tooth .
+  ?procedurei rdf:type obo:OHD_0000006 .
+  
+  ?toothi obo:BFO_0000050 ?patienti . 
+  ?surfacei obo:BFO_0000050 ?toothi . 
+  ?procedurei obo:BFO_0000057 ?toothi .
+   ?procedurei obo:BFO_0000057 ?surfacei .
+  
+  ?patienti rdfs:label ?patient .
+  ?toothi rdfs:label ?tooth .
+  ?surfacei rdfs:label ?surface .
+  ?procedurei rdfs:label ?procedure .
+}
+group by ?patient ?tooth ?surface ?procedure
+order by desc(?total) ?patient ?tooth ?surface ?procedure
+limit 5
+"
+
+	  :use-reasoner (eval 'owlim-se-r21)
+	  :trace "count of restorative procedures on same tooth and surface")
+
+  nil)
+
+(defun filling-procedures-on-patient-3256-query ()
+  (sparql 
+"
+PREFIX :<http://purl.obolibrary.org/obo/ohd/dev/ohd.owl#>
+PREFIX geo-pos:<http://www.w3.org/2003/01/geo/wgs84_pos#>
+PREFIX umbel-ac:<http://umbel.org/umbel/ac/>
+PREFIX sw-vocab:<http://www.w3.org/2003/06/sw-vocab-status/ns#>
+PREFIX ff:<http://factforge.net/>
+PREFIX music-ont:<http://purl.org/ontology/mo/>
+PREFIX dc-term:<http://purl.org/dc/terms/>
+PREFIX om:<http://www.ontotext.com/owlim/>
+PREFIX opencyc-en:<http://sw.opencyc.org/2008/06/10/concept/en/>
+PREFIX factbook:<http://www.daml.org/2001/12/factbook/factbook-ont#>
+PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX pext:<http://proton.semanticweb.org/protonext#>
+PREFIX dc:<http://purl.org/dc/elements/1.1/>
+PREFIX ot:<http://www.ontotext.com/>
+PREFIX onto:<http://www.ontotext.com/>
+PREFIX protege:<http://protege.stanford.edu/plugins/owl/protege#>
+PREFIX foaf:<http://xmlns.com/foaf/0.1/>
+PREFIX yago:<http://mpii.de/yago/resource/>
+PREFIX daml:<http://www.daml.org/2001/03/daml+oil#>
+PREFIX umbel:<http://umbel.org/umbel#>
+PREFIX pkm:<http://proton.semanticweb.org/protonkm#>
+PREFIX wordnet16:<http://xmlns.com/wordnet/1.6/>
+PREFIX swrlb:<http://www.w3.org/2003/11/swrlb#>
+PREFIX owl:<http://www.w3.org/2002/07/owl#>
+PREFIX example:<http://example.com/>
+PREFIX gr:<http://purl.org/goodrelations/v1#>
+PREFIX wordnet:<http://www.w3.org/2006/03/wn/wn20/instances/>
+PREFIX opencyc:<http://sw.opencyc.org/concept/>
+PREFIX ro:<http://www.obofoundry.org/ro/ro.owl#>
+PREFIX wordn-sc:<http://www.w3.org/2006/03/wn/wn20/schema/>
+PREFIX nytimes:<http://data.nytimes.com/>
+PREFIX dbp-prop:<http://dbpedia.org/property/>
+PREFIX owl2:<http://www.w3.org/2006/12/owl2#>
+PREFIX geonames:<http://sws.geonames.org/>
+PREFIX dcterms:<http://purl.org/dc/terms/>
+PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
+PREFIX swrl:<http://www.w3.org/2003/11/swrl#>
+PREFIX owl2xml:<http://www.w3.org/2006/12/owl2-xml#>
+PREFIX dbpedia:<http://dbpedia.org/resource/>
+PREFIX oasis:<http://psi.oasis-open.org/iso/639/#>
+PREFIX individuals:<http://purl.obolibrary.org/obo/ohd/individuals/>
+PREFIX geo-ont:<http://www.geonames.org/ontology#>
+PREFIX umbel-en:<http://umbel.org/umbel/ne/wikipedia/>
+PREFIX xsp:<http://www.owl-ontologies.com/2005/08/07/xsp.owl#>
+PREFIX bbc-pont:<http://purl.org/ontology/po/>
+PREFIX ptop:<http://proton.semanticweb.org/protontop#>
+PREFIX lingvoj:<http://www.lingvoj.org/ontology#>
+PREFIX fb:<http://rdf.freebase.com/ns/>
+PREFIX dbtune:<http://dbtune.org/bbc/peel/work/>
+PREFIX obo:<http://purl.obolibrary.org/obo/>
+PREFIX psys:<http://proton.semanticweb.org/protonsys#>
+PREFIX umbel-sc:<http://umbel.org/umbel/sc/>
+PREFIX dbp-ont:<http://dbpedia.org/ontology/>
+PREFIX ub:<http://www.lehigh.edu/~zhp2/2004/0401/univ-bench.owl#>
+PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>
+PREFIX oboInOwl:<http://www.geneontology.org/formats/oboInOwl#>
+PREFIX skos:<http://www.w3.org/2004/02/skos/core#>
+
+select ?patient ?tooth ?surface ?procedure ?date
+where {
+  ?patienti rdf:type obo:OHD_0000012 .
+  ?toothi rdf:type obo:FMA_12516 .
+  ?surfacei rdf:type obo:FMA_no_fmaid_Surface_enamel_of_tooth .
+  ?procedurei rdf:type obo:OHD_0000006 .
+  
+  ?toothi obo:BFO_0000050 ?patienti . 
+  ?surfacei obo:BFO_0000050 ?toothi . 
+  ?procedurei obo:BFO_0000057 ?toothi .
+  ?procedurei obo:BFO_0000057 ?surfacei .
+  ?procedurei obo:OHD_0000015 ?date .
+  
+  ?patienti rdfs:label ?patient .
+  ?toothi rdfs:label ?tooth .
+  ?surfacei rdfs:label ?surface .
+  ?procedurei rdfs:label ?procedure .
+filter (str(?patient) = \"patient 3256\")
+filter (str(?tooth) = \"tooth 30 of patient 3256\")
+filter (str(?surface) = \"mesial surface of tooth 30 in patient 3256\")
+}
+order by ?patient ?tooth ?surface ?procedure ?date
+"
+
+	  :use-reasoner (eval 'owlim-se-r21)
+	  :trace "restorative procedures on mesial surface of tooth 30 in patient 3256")
   nil)
