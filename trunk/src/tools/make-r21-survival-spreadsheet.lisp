2d68b6a86fe933b2970344f46b55659048a4531c
wdduncan@gmail.com <wdduncan@gmail.com@b0be31cf-4e2f-eb6c-0eff-db977a60bcf5>
2012-10-10 02:51:37 +0000

    updating progress

--- trunk/src/tools/make-r21-survival-spreadsheet.lisp
+++ trunk/src/tools/make-r21-survival-spreadsheet.lisp
@@ -467,3 +467,82 @@ order by ?patient ?tooth ?surface ?procedure ?date
 	  :use-reasoner (eval 'owlim-se-r21)
 	  :trace "restorative procedures on mesial surface of tooth 30 in patient 3256")
   nil)
+
+(defun ada-code-counts-query ()
+
+  (sparql 
+"
+PREFIX :<http://purl.obolibrary.org/obo/ohd/dev/ohd.owl#>
+PREFIX geo-pos:<http://www.w3.org/2003/01/geo/wgs84_pos#>
+PREFIX umbel-ac:<http://umbel.org/umbel/ac/>
+PREFIX sw-vocab:<http://www.w3.org/2003/06/sw-vocab-status/ns#>
+PREFIX ff:<http://factforge.net/>
+PREFIX music-ont:<http://purl.org/ontology/mo/>
+PREFIX dc-term:<http://purl.org/dc/terms/>
+PREFIX om:<http://www.ontotext.com/owlim/>
+PREFIX opencyc-en:<http://sw.opencyc.org/2008/06/10/concept/en/>
+PREFIX factbook:<http://www.daml.org/2001/12/factbook/factbook-ont#>
+PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX pext:<http://proton.semanticweb.org/protonext#>
+PREFIX dc:<http://purl.org/dc/elements/1.1/>
+PREFIX ot:<http://www.ontotext.com/>
+PREFIX onto:<http://www.ontotext.com/>
+PREFIX protege:<http://protege.stanford.edu/plugins/owl/protege#>
+PREFIX foaf:<http://xmlns.com/foaf/0.1/>
+PREFIX yago:<http://mpii.de/yago/resource/>
+PREFIX daml:<http://www.daml.org/2001/03/daml+oil#>
+PREFIX umbel:<http://umbel.org/umbel#>
+PREFIX pkm:<http://proton.semanticweb.org/protonkm#>
+PREFIX wordnet16:<http://xmlns.com/wordnet/1.6/>
+PREFIX swrlb:<http://www.w3.org/2003/11/swrlb#>
+PREFIX owl:<http://www.w3.org/2002/07/owl#>
+PREFIX example:<http://example.com/>
+PREFIX gr:<http://purl.org/goodrelations/v1#>
+PREFIX wordnet:<http://www.w3.org/2006/03/wn/wn20/instances/>
+PREFIX opencyc:<http://sw.opencyc.org/concept/>
+PREFIX ro:<http://www.obofoundry.org/ro/ro.owl#>
+PREFIX wordn-sc:<http://www.w3.org/2006/03/wn/wn20/schema/>
+PREFIX nytimes:<http://data.nytimes.com/>
+PREFIX dbp-prop:<http://dbpedia.org/property/>
+PREFIX owl2:<http://www.w3.org/2006/12/owl2#>
+PREFIX geonames:<http://sws.geonames.org/>
+PREFIX dcterms:<http://purl.org/dc/terms/>
+PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
+PREFIX swrl:<http://www.w3.org/2003/11/swrl#>
+PREFIX owl2xml:<http://www.w3.org/2006/12/owl2-xml#>
+PREFIX dbpedia:<http://dbpedia.org/resource/>
+PREFIX oasis:<http://psi.oasis-open.org/iso/639/#>
+PREFIX individuals:<http://purl.obolibrary.org/obo/ohd/individuals/>
+PREFIX geo-ont:<http://www.geonames.org/ontology#>
+PREFIX umbel-en:<http://umbel.org/umbel/ne/wikipedia/>
+PREFIX xsp:<http://www.owl-ontologies.com/2005/08/07/xsp.owl#>
+PREFIX bbc-pont:<http://purl.org/ontology/po/>
+PREFIX ptop:<http://proton.semanticweb.org/protontop#>
+PREFIX lingvoj:<http://www.lingvoj.org/ontology#>
+PREFIX fb:<http://rdf.freebase.com/ns/>
+PREFIX dbtune:<http://dbtune.org/bbc/peel/work/>
+PREFIX obo:<http://purl.obolibrary.org/obo/>
+PREFIX psys:<http://proton.semanticweb.org/protonsys#>
+PREFIX umbel-sc:<http://umbel.org/umbel/sc/>
+PREFIX dbp-ont:<http://dbpedia.org/ontology/>
+PREFIX ub:<http://www.lehigh.edu/~zhp2/2004/0401/univ-bench.owl#>
+PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>
+PREFIX oboInOwl:<http://www.geneontology.org/formats/oboInOwl#>
+PREFIX skos:<http://www.w3.org/2004/02/skos/core#>
+
+select ?adacode (count(*) as ?total)
+where {
+  ?adacodetype rdfs:subClassOf obo:CDT_1000001 . 
+  ?adacodei rdf:type ?adacodetype .
+  ?adacodei obo:OHD_0000092 ?adacodetype . # obo:OHD_0000092 -> 'asserted type' 
+  ?adacodei rdfs:label ?adacode .
+
+}
+group by ?adacode
+order by desc(?total)
+"
+
+	  :use-reasoner (eval 'owlim-se-r21)
+	  :trace "count of restorative procedures on same tooth and surface")
+
+  nil)
