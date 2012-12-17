d0604bd4a7df14efaff00b59f5d2adfdf87a68b4
wdduncan <wdduncan@gmail.com>
2012-12-17 21:57:09 +0000

    updating progress

--- src/tools/eaglesoft-query-all.r
+++ src/tools/eaglesoft-query-all.r
@@ -1,5 +1,45 @@
 source("eaglesoft-load-r-variables.r")
 
+## function for writing data
+write.caplan.data <- function(results) {
+  ## get info about first row
+  row <- results[1, ]
+  ##print(row)
+
+  ## get info about 2nd through next to last row
+  for (i in 2:(nrow(results) - 1)) {
+    temprow <- results[i, ]
+    
+    ##print(temprow)
+
+    ## compare patient ids (index 1) and tooth (index 4) between row and temprow
+    ## if they match then "append" info to row
+    ## otherwise write to file and set row to temprow (this moves the row forward)
+    if (row[1] == temprow[1] && row[4] == temprow[4]) {
+      row <- c(row, temprow[5:18])
+    } else {
+      ## note: row need to be transposed
+      write.table(t(row), file="~/Desktop/r21.txt", sep="\t",
+                  col.names=FALSE, row.names=FALSE, append=TRUE)
+      row <- temprow
+    }
+  }
+  ## get info about last row
+  temprow <- res.caplan[nrow(res.caplan), ]
+  
+  if (row[1] == temprow[1] && row[4] == temprow[4]) {
+    row <- c(row, temprow[5:18])
+  } else {
+    row <- temprow
+  }
+
+  ## write last row
+  ## note: row need to be transposed
+  write.table(t(row), file="~/Desktop/r21.txt", sep="\t",
+              col.names=FALSE, row.names=FALSE, append=TRUE)
+}
+
+
 ## column names in Caplan spreadsheet
 column.names <- c("patientid", "sex", "birthdate", "tthnum", "procdate","procclass", "proccode",
                   "matm", "mato", "matd", "matf", "matl","dxm", "dxo", "dxd", "dxf", "dxl", "provider")
@@ -13,11 +53,11 @@ res.column.names <- colnames(res)
 
 ## dates in res are in form "YYY-MM-DD^^http://www.w3.org/2001/XMLSchema#date"
 ## so I need to lop off the ^^http://www.w3.org/2001/XMLSchema#date part
-if (grep("birthdate", res.column.names) > 0) {
+  if (grep("birthdate", res.column.names) > 0) {
   res[ , "birthdate"] <- substring(res[ , "birthdate"], 1, 10)
 }
-
-if (grep("procdate", res.column.names) > 0) {
+  
+  if (grep("procdate", res.column.names) > 0) {
   res[ , "procdate"] <- substring(res[ , "procdate"], 1, 10)
 }
         
@@ -50,12 +90,9 @@ res.ordered[, "procdate"] <-
         substring(res.ordered[, "procdate"], 1, 4), sep="")
 
 ## order columns to match Caplan's spreadsheet
-res.ordered <- res.ordered[, c("patientid", "sex", "birthdate", "tthnum", "procdate","procclass","proccode",
-                                    "matm", "mato", "matd", "matf", "matl","dxm", "dxo", "dxd", "dxf", "dxl", "provider")]
+res.ordered <- res.ordered[, c("patientid", "sex", "birthdate", "tthnum", "procdate","procclass",
+                               "proccode", "matm", "mato", "matd", "matf", "matl",
+                               "dxm", "dxo", "dxd", "dxf", "dxl", "provider")]
 
-## write $results of dataframe to SAS file.  So, if my dataframe is df, the call would look like:
-## library(foreign)
-## write.foreign(res.ordered, "~/Desktop/r21.txt", "~/Desktop/r21.sas", package="SAS")
-
-## also do this
-## write.table(res.ordered, file="~/Desktop/r21/r21.txt", sep="\t", col.names=FALSE, row.names=FALSE)
+## write results to file
+write.caplan.data(res.ordered)
