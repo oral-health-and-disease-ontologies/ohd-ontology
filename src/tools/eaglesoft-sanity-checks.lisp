081c22d9bb3397f515fcb6a0881ff3d1b4fe6cf4
wdduncan <wdduncan@gmail.com>
2013-08-29 20:04:49 +0000

    This file contains queries (and perhaps other checks) against both the triple-store and eaglesoft database to check the "sanity" of the triple store.

---
+++ src/tools/eaglesoft-sanity-checks.lisp
@@ -0,0 +1,4 @@
+;; Creator: Bill Duncan
+;; Creation Date: Aug. 29, 2013
+;; This file contains queries (and perhaps other checks) against both the triple-store and eaglesoft database
+;; to check the "sanity" of the triple store
