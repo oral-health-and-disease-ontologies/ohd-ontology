c277368726a8c4d5b6bb195b4ce5ad87aefc9b91
wdduncan@gmail.com <wdduncan@gmail.com@b0be31cf-4e2f-eb6c-0eff-db977a60bcf5>
2013-08-30 19:50:22 +0000

    update

--- trunk/src/tools/eaglesoft-sanity-checks.lisp
+++ trunk/src/tools/eaglesoft-sanity-checks.lisp
@@ -1,4 +1,51 @@
-;; Creator: Bill Duncan
-;; Creation Date: Aug. 29, 2013
-;; This file contains queries (and perhaps other checks) against both the triple-store and eaglesoft database
-;; to check the "sanity" of the triple store
+#|
+Creator: Bill Duncan
+Creation Date: Aug. 29, 2013
+This file contains queries (and perhaps other checks) against both the triple-store and eaglesoft database
+to check the "sanity" of the triple store.
+
+For preliminary checks, I ran a number of queries to verify that the three tables from which the patient_history
+is built (transactions, existing_conditions, patient_conditions) have their respective date info:
+
+select * from transactions where tran_date is null -- no rows found
+select * from existing_services where date_completed is null -- no rows found
+select * from existing_services where date_entered is null -- no rows found
+select * from patient_conditions where date_entered is null -- no rows found
+
+select * from transactions where length(tran_date) = 0 -- no rows found
+select * from existing_services where length(date_completed) = 0 -- no rows found
+select * from existing_services where length(date_entered) = 0 -- no rows found
+select * from patient_conditions where length(date_entered) = 0 -- no rows found
+
+|#
+
+(defun check-patient-visits-for-date (&key patient-id visit-date)
+  "Check that the number of visits in the ontology matches the database"
+(let ((query-string nil)
+      (sql-result nil)
+      (sparql-result))
+
+  ;; build sql string
+  (setf query-string "
+select * ")
+
+
+;; develop sparql string
+	
+(setf query-string "
+select (count (*) as ?X) where {
+?x rdf:type dental_visit: .
+?x rdfs:label ?s .
+} limit 10 ")	
+
+
+)
+
+(defun sparql-prefixes ()
+  
+)
+
+(defun print-ohd-sparql-prefixes ()
+  ;; do a sparql query to pull out ohd iris with lables
+  ;; and parse together prefix list
+)
