1be2c44b25b86a98c42e1d99ea6bc92b2c8f83e4
wdduncan <wdduncan@gmail.com>
2015-03-10 19:03:52 +0000

    working on query for nonfailed restorations

--- src/analysis/sparql-restoration-queries.r
+++ src/analysis/sparql-restoration-queries.r
@@ -35,6 +35,7 @@ group by ?toothtype
   ## return query string
   query.string
 }
+
 first.and.second.restoration.query.string <- function (limit=0)
 {
   query.string <- "
@@ -134,7 +135,7 @@ first.and.second.restoration.query.string <- function (limit=0)
   ?proci1 rdfs:label ?procedure1 .
   ?proci2 rdfs:label ?procedure2 .
   
-  ## we only those records where the in between date (?date3)
+  ## we only want those records where the in between date (?date3)
   ## is not bound, this gives us adjacent dates
   filter (!bound(?date3))
 }
@@ -149,3 +150,130 @@ first.and.second.restoration.query.string <- function (limit=0)
 
 query.string
 }
+
+first.and.second.amalgam.restoration.query.string <- function (limit=0)
+{
+  query.string <- "
+  select distinct ?patienttype ?birthdate ?tooth ?toothtype ?surface ?surfacetype ?procedure1 ?date1 ?procedure2 ?date2
+  
+  ## look for surface specific for now. Find two procedures and an optional third
+  where
+{
+  ## patient's sex and birth date
+  ?patienti rdf:type patient: .
+  ?patienti asserted_type: ?patienttypei .
+  ?patienti birth_date: ?birthdate .
+  
+  ## patient's tooth & tooth type
+  ?toothi rdf:type tooth: .
+  ?toothi asserted_type: ?toothtypei .
+  ?toothi is_part_of: ?patienti .
+  
+  ## surfaces and their types that are part of tooth
+  ?surfacei rdf:type tooth_surface: .
+  ?surfacei asserted_type: ?surfacetypei .
+  ?surfacei is_part_of: ?toothi .
+  
+  ##- get restoration procedure in general
+  ## this is done by finding the procedures that realize
+  ## some tooth to be resotred role that is borne by the tooth
+  ## the procedures occurrence date (?date) is used to determine
+  ## the first restoration date by taking the min of ?date
+  ## see having clause below
+  ##### ?proci rdf:type tooth_restoration_procedure: . ******
+  ?proci rdf:type <http://purl.obolibrary.org/obo/OHD_0000041> . ## amalagam procedure
+  ?rolei rdf:type tooth_to_be_restored_role: .
+  
+  ## the tooth to be restored role inheres in the tooth
+  ## and is realized by the procedure
+  ?rolei inheres_in: ?toothi .
+  ?proci realizes: ?rolei .
+  ?proci occurrence_date: ?date . # date of procedure
+  
+  ##- first procedure: the tooth is same as the general procedure above.
+  ## the first procedure is determined in a manner similar to the
+  ## general procedure above
+  ####### ?proci1 rdf:type tooth_restoration_procedure: . ******
+  ?proci1 rdf:type <http://purl.obolibrary.org/obo/OHD_0000041> . ## amalagam procedure
+  ?rolei1 rdf:type tooth_to_be_restored_role: .
+  
+  ## the tooth to be restored role inheres in the tooth
+  ## and is realized by the procedure
+  ?rolei1 inheres_in: ?toothi .
+  ?proci1 realizes: ?rolei1 .
+  ?proci1 occurrence_date: ?date1 . # date of procedure 1
+  
+  ## surfaces that have been restored particpate in the procedure
+  ?proci1 has_participant: ?surfacei .
+  
+  ##- second process: the tooth and surface remain the same as the first
+  ## but a new process that realizes a new role is searched for
+  ?proci2 rdf:type tooth_restoration_procedure: .
+  ?rolei2 rdf:type tooth_to_be_restored_role: .
+  
+  ## the tooth to be restored role inheres in the tooth
+  ## and is realized by the procedure
+  ?rolei2 inheres_in: ?toothi .
+  ?proci2 realizes: ?rolei2 .
+  ?proci2 occurrence_date: ?date2 . # date fo procedure 2
+  
+  ## surfaces that have been restored particpate in the procedure
+  ?proci2 has_participant: ?surfacei .
+  
+  ## we only those second procedure that are after the first
+  filter (?date2 > ?date1 && ?proci1 != ?proci2)
+  
+  ##- third process: the tooth and surface remain the same,
+  ## but the date is between the other two
+  ## (we check later that this doesn't succeed)
+  optional {
+  ?proci3 rdf:type tooth_restoration_procedure: .
+  ?rolei3 rdf:type tooth_to_be_restored_role: .
+  
+  ## the tooth to be restored role inheres in the tooth
+  ## and is realized by the procedure
+  ?rolei3 inheres_in: ?toothi .
+  ?proci3 realizes: ?rolei3 .
+  ?proci3 occurrence_date: ?date3 . # date of procedure 3
+  
+  ## surfaces that have been restored particpate in the procedure
+  ?proci3 has_participant: ?surfacei .
+  
+  ## we want only that procedures that are between
+  ## two other procedures
+  filter (?date3<?date2 && ?date3>?date1)}
+  
+  ## assign labels
+  ?patienttypei rdfs:label ?patienttype .
+  ?toothi rdfs:label ?tooth .
+  ?toothtypei tooth_number: ?toothtype .
+  ?surfacei rdfs:label ?surface .
+  ?surfacetypei rdfs:label ?surfacetype .
+  ?proci1 rdfs:label ?procedure1 .
+  ?proci2 rdfs:label ?procedure2 .
+  
+  ## we only want those records where the in between date (?date3)
+  ## is not bound, this gives us adjacent dates
+  filter (!bound(?date3))
+}
+  group by ?patienttype ?birthdate ?tooth ?toothtype ?surface ?surfacetype ?procedure1 ?date1 ?procedure2 ?date2
+  ## match the min and first procedure dates
+  having (?date1 = min(?date))
+  order by ?tooth ?surface ?date1
+  
+  "
+  if (limit > 0)
+     query.string <- paste(query.string,"limit ", limit, "\n", sep="")
+
+query.string
+}
+
+nonfailed.restorations.count.query.string <- function(limit = 0) {
+  query.string <- ""
+  
+  if (limit > 0)
+    query.string <- paste(query.string,"limit ", limit, "\n", sep="")
+  
+  query.string
+}
+
