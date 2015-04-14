f669f4ce935861e3f5355f753d209374ca17a052
alanruttenberg@gmail.com <alanruttenberg@gmail.com@b0be31cf-4e2f-eb6c-0eff-db977a60bcf5>
2015-04-14 20:58:14 +0000

    fix packages install

--- trunk/src/analysis/packages.r
+++ trunk/src/analysis/packages.r
@@ -1,7 +1,16 @@
-install_if_necessary <- function (package)
-{ if (!require(package,character.only = TRUE)) { install.packages(package) } }
+
+install_if_necessary <- function (package,from=NULL)
+{ if (!require(package,character.only = TRUE)) 
+    { if (is.Null(from))
+        { install.packages(package); require(package) }
+    else
+      { install.packages(c((paste(getwd(),"/",from,sep="")),repos=NULL),type="source") }
+    }
+}
 
 install_if_necessary("rJava")
+install_if_necessary("rrdflibs","rrdflibs_1.3.0.tar.gz")
+install_if_necessary("rrdf","rrdf_2.0.2.tar.gz")
 install_if_necessary("ggthemes")
 install_if_necessary("Cairo")
 install_if_necessary("gridExtra")
