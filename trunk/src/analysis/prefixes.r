c8490b946d4b2ef373c61d07d8fa8acb63deaff5
alanruttenberg@gmail.com <alanruttenberg@gmail.com@b0be31cf-4e2f-eb6c-0eff-db977a60bcf5>
2015-04-23 23:28:15 +0000

    queryw is like queryc but sends the sparql to the workbench, which must be the active tab in chrome

--- trunk/src/analysis/prefixes.r
+++ trunk/src/analysis/prefixes.r
@@ -97,3 +97,8 @@ prefixes_for_sparql <- function(query)
 
 ## to remove PREFIX gsub("PREFIX[ ]*[^\n]*\n","",querystring(q))
 
+setRepositoryPrefixes <- function()
+  { DELETE(paste0(current_endpoint,"/namespaces"));
+    lapply(ls(prefixes),function(p) {PUT(paste0(current_endpoint,"/namespaces/",gsub(":","",p)),body=gsub("<|>","",get(p,prefixes)))});
+    NULL
+  }
