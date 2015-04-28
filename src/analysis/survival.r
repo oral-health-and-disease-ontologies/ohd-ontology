19da89074931d715dcfe25ec321251ba7fb49e9a
alanruttenberg <alanruttenberg@gmail.com>
2015-04-28 21:25:30 +0000

    Survival analysis by gender, by age group

--- src/analysis/survival.r
+++ src/analysis/survival.r
@@ -15,7 +15,6 @@ survival_posterior_vs_anterior <- function ()
     { s <- rsurv()
       s$all <- collect_all_restorations_and_latest_followup()
       s$fail <- collect_restoration_failures()
-      postprocess_anterior_posterior(s);
       create_surv(s)
   }
 
@@ -32,61 +31,61 @@ subset_all_minus_fail <- function(all, fail)
 
 create_surv <- function (s)
     { 
+        lastsa <<- s;
         no_record_of_failure <- NULL;
 
         difference_in_months <-
             function(start,end)
                 { length(seq(as.Date(start),as.Date(end),by="month"))-1 }
-
+        difference_in_years <-
+            function(start,end)
+                { length(seq(as.Date(start),as.Date(end),by="year"))-1 }
         no_record_of_failure <- subset_all_minus_fail(s$all,s$fail);
         s$have_last_visit <- no_record_of_failure[!is.na(no_record_of_failure[,"latest_date2"]),];
         s$no_record_of_failure_in_months <-
             mapply(difference_in_months,s$have_last_visit[,"date1"],s$have_last_visit[,"latest_date2"])
-
+        s$age_at_last_visit <- mapply(difference_in_years,s$have_last_visit[,"birthdate"],s$have_last_visit[,"date1"])
+        
         s$ncensor = length(s$no_record_of_failure_in_months);
         
         # should interval censor
         s$fail_difference_in_months <-
             mapply(difference_in_months,s$fail[,"date1"],s$fail[,"soonest_date2"])
+        s$age_at_fail <- mapply(difference_in_years,s$fail[,"birthdate"],s$fail[,"date1"])
 
         s$nfail <- length(s$fail_difference_in_months);
         
         s$status <- c(rep(1,s$nfail),rep(0,s$ncensor));
         s$months <- c(s$fail_difference_in_months,s$no_record_of_failure_in_months)
+        
+        s$correlates <- rbind(data.frame(location=s$fail[,"tooth_type"],
+                                         gender=s$fail[,"gender"],
+                                         age=s$age_at_fail),
+                              data.frame(location=s$have_last_visit[,"tooth_type"],
+                                         gender=s$have_last_visit[,"gender"],
+                                         age=s$age_at_last_visit))
 
-        s$correlates <- rbind(data.frame(location=s$fail[,"is_posterior"]),
-                               data.frame(location=s$have_last_visit[,"is_posterior"]))
-
+        lastsa <<- s;
+        s$correlates[,"age_group"] <- cut(s$correlates[,"age"],breaks=c(0,30,100));
         s$nlastvisit <- dim(s$have_last_visit)[1]
 
         surv <- Surv(s$months,s$status) ;
         s$surv <- surv;
-        s$fit <- survfit(surv~location,conf.type="none",data=s$correlates);
+        s$location_fit <- survfit(surv~location,conf.type="none",data=s$correlates);
+        s$gender_fit <- survfit(surv~gender,conf.type="none",data=s$correlates);
+        s$age_fit <- survfit(surv~age_group,conf.type="none",data=s$correlates);
         s
     }
 
-
-postprocess_anterior_posterior <- function(s)
-    {
-        eval.parent(substitute(
-            s$all[is.na(s$all[,"is_posterior"]),"is_posterior" ]<-"anterior"));
-        eval.parent(substitute(
-            s$fail[is.na(s$fail[,"is_posterior"]),"is_posterior" ]<-"anterior"));
-        eval.parent(substitute(
-            s$all[s$all[,"is_posterior"]==1,"is_posterior" ]<- "posterior"));
-        eval.parent(substitute(
-            s$fail[s$fail[,"is_posterior"]==1,"is_posterior" ]<- "posterior"));
-    }
-   
-
-plot.rsurv <- function (s)
+plot.rsurv <- function (s,fit=s$location_fit)
     {
         if (file.exists("/tmp/rsvg.svg")) { file.remove("/tmp/rsvg.svg") }
         svg(filename="/tmp/rsvg.svg")
-        plot = ggsurv(s$fit) +
-            guides(linetype = F) +
-                scale_colour_discrete(name = 'Location', breaks = c("anterior","posterior"), 
-                                      labels=c('Posterior', 'Anterior'))
+        plot = ggsurv(fit,cens.col='gray',plot.cens=F,back.white=T)
+ #+
+        #guides(linetype = F) +
+         #       scale_colour_discrete(name = 'Location', breaks = c("anterior","posterior"), 
+          #                            labels=c('Posterior', 'Anterior'))
         print(plot)
         dev.off()
         browseURL("file:///tmp/rsvg.svg")
