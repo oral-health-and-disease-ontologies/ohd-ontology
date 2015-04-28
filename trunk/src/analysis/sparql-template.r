c5c2e1b1213a7f75a8a5eb628810bc4cd87df448
alanruttenberg@gmail.com <alanruttenberg@gmail.com@b0be31cf-4e2f-eb6c-0eff-db977a60bcf5>
2015-04-28 19:39:55 +0000

    particularly obnoxious debugging session with R

--- trunk/src/analysis/sparql-template.r
+++ trunk/src/analysis/sparql-template.r
@@ -16,60 +16,46 @@ tb <- function(...) { paste(...,"\n",sep="\n")}
 
 sparql_interpolate <- function(...) 
     {
-       # construct the pattern string - a basic graph pattern
-       bgp=paste(...,"",sep="\n")
-       # get the call to the pattern invoker
-       call<-sys.call(sys.parent(1));
-       # extract the arguments
-       argnames <- names(call);
-       argnames <- argnames[argnames!=""]
-       # get its environment so we can evaluate the arguments
-       env <- sys.frame(sys.parent(2))
-       # and do so
-       values <- lapply(argnames,FUN=function(name) { eval(call[[name]],env)})
-       # all arguments are potential replacements until we remove the
-       # ones that don't have values starting with "?"
-       potential <- cbind(argnames,values)
-       # which values start with "?"
-       which<- substr(values,start=1,stop=1)=="?"|| substr(values,start=nchar(values),stop=nchar(values))==":"
-       if (length(which)==0) { return(bgp) }
-       # remove the others
-       actual<- as.matrix(potential[which,])
-       # Now substitute the values into the bgp
-       #
-       substitute_sparql_variable <- 
-         function(old,replacement)
-           { if (substr(replacement,start=1,stop=1)=="?")
-               { pattern <- paste0("(\\?",old,")\\b") }
-             else
-               { pattern <- paste0("(\\b",old,"[:])") }
-             bgp<<- gsub(pattern,replacement,bgp,perl=TRUE)
-           }
-       # I hate R. If there are two arguments the columns are
-       # arguments. If there is one it is a rowname
-       if (dim(actual)[2] == 1)
-         { substitute_sparql_variable(actual[1],actual[2]) }
-       else
-         {  mapply(substitute_sparql_variable,actual[,"argnames"],actual[,"values"]) }
-       make_blanks_unique(bgp);
-   }
-
+        cat("spi:",...,"\n");
+        sbgp<-paste(...,"",sep="\n")
+        call<-sys.call(sys.parent(1));
+        argnames1 <- names(call);
+        argnames <- argnames1[argnames1!=""]
+        env <- sys.frame(sys.parent(2))
+        values <- unlist(lapply(argnames,FUN=function(name) { eval(call[[name]],env)}))
+        potential <- cbind(argnames,values)
+        whichones<- (substr(values,start=1,stop=1)=="?"|| substr(values,start=nchar(values),stop=nchar(values))==":");
+        if (length(argnames)==0) { return(sbgp) };
+        actual<- potential[whichones,]
+        substitute_sparql_variable <- 
+            function(old,replacement)
+                { if (substr(replacement,start=1,stop=1)=="?")
+                      { pattern <- paste0("(\\?",old,")\\b") }
+                  else
+                      { pattern <- paste0("(\\b",old,"[:])") };
+                  sbgp <- gsub(pattern,replacement,sbgp,perl=TRUE)
+                  sbgp <<- sbgp
+              }
+                if (length(argnames) == 1)
+            { substitute_sparql_variable(argnames,values) }
+        else
+            { uu <<- actual;
+              mapply(substitute_sparql_variable,actual[,"argnames"],actual[,"values"]) }
+        make_blanks_unique(sbgp);
+    }
 
 make_blanks_unique <- function (bgp)
     { to_be_replaced<-unique(regmatches(bgp,gregexpr("(?s)_\\:([A-Za-z0-9_]*)",bgp,perl=TRUE))[[1]]);
       to_be_replaced<-rev(to_be_replaced[order(nchar(to_be_replaced), to_be_replaced)])
       count<-gensymcounter;
       gensymcounter <<- gensymcounter + 1
-      { replace_w_numbered <- 
-               function(string)
-                   {replacement <- paste0(string,count)
-                    #cat("before->",bgp,",",replacement,",",string,"\n")
-                    bgp<<- gsub(string,replacement,bgp,perl=TRUE)
-                    #cat("after->",bgp,",",replacement,"\n")
-                }
-        lapply(to_be_replaced,replace_w_numbered)
-        bgp}      
-    }
+      replace_w_numbered <- 
+          function(string)
+              {replacement <- paste0(string,count)
+               bgp<- gsub(string,replacement,bgp,perl=TRUE)
+           }
+      lapply(to_be_replaced,replace_w_numbered)
+      bgp}      
 
 gensymcounter <- 1;
 reset_var_counter <- function()
