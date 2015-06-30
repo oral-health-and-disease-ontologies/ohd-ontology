0a5de4cc287b3cf7fb9092e88ecb7061a94348ee
wdduncan@gmail.com <wdduncan@gmail.com@b0be31cf-4e2f-eb6c-0eff-db977a60bcf5>
2015-06-30 20:13:08 +0000

    setting up directory structure for vdw translation

--- trunk/src/vdw-translation/general-library/load-general-library.lisp
+++
@@ -1,8 +0,0 @@
-
-;; call the asdf to load the general-library
-;; this is in a separate lisp file so that other operations can 
-;; more easily be added (if need be)
-
-;; not sure if I need to compile before load
-(asdf:oos 'asdf:compile-op :general-library)
-(asdf:oos 'asdf:load-op :general-library)
--- trunk/src/vdw-translation/general-library/utils/hash-table-utils.lisp
+++
@@ -1,9 +0,0 @@
-(defun print-hash-table (hash-table &key limit)
-  "prints out the key/value pairs in a hash table; the param limit limits the number of entries printed."
-  (let ((count 0))
-    (loop 
-       for k being the hash-keys in hash-table using (hash-value v) do
-	 (when limit 
-	   (if (>= count limit) (return)))
-         (format t "~a ~a ~%" k v)
-	 (incf count))))
