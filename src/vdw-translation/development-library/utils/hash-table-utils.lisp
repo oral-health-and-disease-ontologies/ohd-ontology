9738b1702e4e10317b9b12b1741fa045753d4253
Bill Duncan <wdduncan@gmail.com>
2015-09-12 23:32:55 -0400

    add code for saving hash tables

--- src/vdw-translation/development-library/utils/hash-table-utils.lisp
+++ src/vdw-translation/development-library/utils/hash-table-utils.lisp
@@ -1,9 +1,46 @@
-(defun print-hash-table (hash-table &key limit)
-  "prints out the key/value pairs in a hash table; the param limit limits the number of entries printed."
+(defun print-hash-table (hash-table &key limit print-key print-value)
+  "Prints out the key/value pairs in a hash table.
+The limit parameter limits the number of entries printed.
+The print-key parameter causes only the keys to be printed.
+The print-value parameter causes only the values the to be printed."
+
   (let ((count 0))
     (loop 
        for k being the hash-keys in hash-table using (hash-value v) do
 	 (when limit 
 	   (if (>= count limit) (return)))
-         (format t "~a ~a ~%" k v)
+	 (cond
+	   (print-key ; print only the key
+	    (format t "~a~%" k))
+	   (print-value ; print only the value
+	    (format t "~a~%" v))
+	   (t
+	      ;; note: there is a tab character between the key and value
+	     (format t "~a	~a ~%" k v)))
 	 (incf count))))
+
+(defun save-hash-table (hash-table file-name &key limit save-key save-value)
+  "Saves the hash table specifed by the hash-table parameter to he file specified by file-name.
+The limit parameter limits the number of entries saved.
+The save-key parameter causes only the keys to be saved.
+The save-value parameter causes only the values the to be saved."
+
+  (let ((count 0))
+    (with-open-file (stream file-name
+			    :direction :output
+			    :if-does-not-exist :create
+			    :if-exists :supersede)
+      (print-db file-name)
+      (loop 
+	 for k being the hash-keys in hash-table using (hash-value v) do
+	   (when limit 
+	     (if (>= count limit) (return)))
+	   (cond
+	     (save-key			; print only the key
+	      (format stream "~a~%" k))
+	     (save-value		; print only the value
+	      (format stream "~a~%" v))
+	     (t
+	      ;; note: there is a tab character between the key and value
+	      (format stream "~a	~a ~%" k v)))
+	   (incf count)))))
