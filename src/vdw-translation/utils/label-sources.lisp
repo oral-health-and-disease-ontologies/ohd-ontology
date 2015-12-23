571f552fd73aebda4d911ff93c22451d26ece3c9
Bill Duncan <wdduncan@gmail.com>
2015-12-23 16:18:56 -0500

    add alternative terms in ontology for filling restorations; add code so that altnerative terms are loaded into label-source hash table

--- src/vdw-translation/utils/label-sources.lisp
+++ src/vdw-translation/utils/label-sources.lisp
@@ -9,6 +9,38 @@
 			    "ohd:imports;BFO2;bfo2-classes.owl"
 			    "ohd:imports;BFO2;bfo2-relations.owl")))
 
+(defun add-alternative-terms ()
+  "adds the alternative terms from the ontology to the label-source hash table"
+  (let (table ohd query results)
+    ;; load ohd ontology
+    (setf ohd (load-ontology "ohd:ontology;ohd.owl" :reasoner :none))
+
+    ;; build query string
+    (setf query 
+	  (str+ "select ?term ?uri "
+		"where {?uri <" (uri-full !'alternative term'@ohd) "> ?term . } "))
+    
+    ;; query ontology to get a list of alternative terms and uri
+    (setf results
+	  (sparql query :kb ohd :use-reasoner :none))
+    
+    ;; set a pointer to label2uri hash table
+    (setf table (label2uri *ohd-label-source*))
+
+    ;; add results of sparql query to hash table
+    (loop 
+       for r in results
+       for term = (first r)
+       for uri = (second r)
+       do
+	 (setf (gethash term table) uri))
+
+    ;; return hash table
+    table))
+
+;; ********** add alternative terms to OHD ***********
+(add-alternative-terms )
+
 (defun get-copy-ohd-label-source ()
   "Makes and returns a copy of the *ohd-label-source* hash table created in make-uri-from-label-source."
   (let ((label-copy-ht (make-hash-table :test 'equalp))
@@ -53,10 +85,3 @@
     
     ;; return the diff labels (if any)
     diff-labels))
-
-
-
-
-
-
-
