d50e6273e4973889d9aee8d7d6721debb2539925
Duncan <wdduncan@gmail.com>
2022-10-25 19:06:13 -0400

    add output for extracting fma tooth classes

--- src/ontology/scripts/robot-extract-tooth-classes.sh
+++ src/ontology/scripts/robot-extract-tooth-classes.sh
@@ -2,3 +2,7 @@ robot extract --method MIREOT \
 		--input ../imports/fma-jaws-teeth.owl \
 		--branch-from-term "obo:FMA_12516" \
 		--output fma-teeth.owl
+
+robot query -i fma-teeth.owl -q fma-tooth-classes.rq out-fma-tooth.tmp.tsv
+
+cat out-fma-tooth.tmp.tsv
